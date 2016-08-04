fc51e682c710e90129ec6c7642bad338330d9735
Stephen Dunne III <stephen@icanhazmail.net>
2016-08-04 15:57:20 -0400

    add dailyploop to setup

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
 setup(name='ploopsnapshot',
-      version='0.0.1',
+      version='0.0.2',
       description='Create and track Ploop snapshots.',
       url='http://github.com/nexcess/ploopsnapshot',
       author='Nexcess.net',
       license='Apache 2.0',
       packages=['ploopsnapshot'],
+      scripts = ['bin/dailyploop'],
       zip_safe=False)
